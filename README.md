c2fd21d6a511d4f8d878879d5a2fc83e3000d709
eguber <emanuel.guberovic@gmail.com>
2019-03-22 15:26:50 +0100

    added initial code

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# profile_images_text
-This script allows the user to generate images for user profiles, that can be used in any application as a default placeholder when user hasn't uploaded a custom profile image.
+Generic letters profile image generator.
+
+This script allows the user to generate images for user profiles, that can be used in any application as a default
+placeholder when user hasn't uploaded a custom profile image. The idea is tu have a range of placeholder profile
+images that depend on users initials, making the UX better and richer in comparison to just using a single generic 
+placeholder image.
+
+This script can be used to generate a customizable set of such images - sizes, fonts and colors can be changed to fit
+the application in which generated images can be used.
+
+The core functionality uses PIL library.
+
+This file can also be imported as a module and contains the following
+functions:
+
+    * profile_image_generator - generates a generic user profile image
+
+Todo:
+    * add ability to be called from terminal with a hel function
+    * add more customization options (no circle, circle :) )
+    * more anti-aliasing options
+    * better example
---
+++ profile_image_generator.py
@@ -0,0 +1,89 @@
+from PIL import Image, ImageDraw, ImageFont
+
+""" Generic letters profile image generator.
+
+This script allows the user to generate images for user profiles, that can be used in any application as a default
+placeholder when user hasn't uploaded a custom profile image. The idea is tu have a range of placeholder profile
+images that depend on users initials, making the UX better and richer in comparison to just using a single generic 
+placeholder image.
+
+This script can be used to generate a customizable set of such images - sizes, fonts and colors can be changed to fit
+the application in which generated images can be used.
+
+The core functionality uses PIL library.
+
+This file can also be imported as a module and contains the following
+functions:
+
+    * profile_image_generator - generates a generic user profile image
+
+Todo:
+    * add ability to be called from terminal with a hel function
+    * add more customization options (no circle, circle :) )
+    * more anti-aliasing options
+    * better example
+
+
+"""
+
+
+def generate_profile_image(filename, text, font, text_color, image_dimensions, background_color, circle_padding,
+                           circle_color, type='JPEG', quality=90):
+    """ Generates a generic user profile image.
+
+    Args:
+        filename: generated filename
+        type: generated filetype (check possible image file types defined by PIL module)
+        quality: set a value between 0-100, lower values mean lower quality but lesser file size
+        text: text to be displayed on the image, usually initials
+        font: PIL ImageFont object that defines the displayed text's style
+        text_color: displayed text's color (possible color names defined by PIL modules)
+        image_dimensions: generated image dimensions
+        background_color: generated image background color (possible color names defined by PIL modules)
+        circle_padding: circle in which text is displayed's padding dimensions
+        circle_color: circle in which text is displayed's color
+
+
+    Returns:
+        The return value. True for success, False otherwise.
+
+    """
+
+    img = Image.new('RGB', (image_dimensions[0], image_dimensions[1]), color=background_color)
+    d = ImageDraw.Draw(img)
+    d.ellipse(
+        (circle_padding, circle_padding, image_dimensions[0] - circle_padding, image_dimensions[1] - circle_padding),
+        fill=circle_color)
+
+    text_pos = ((image_dimensions[0] - fnt.getsize(text)[0]) / 2 - circle_padding,
+                (image_dimensions[0] - fnt.getsize(text)[1]) / 2 - circle_padding)
+
+    d.text(text_pos, text, font=font, fill=text_color)
+
+    img = img.resize((700, 700), Image.ANTIALIAS)
+    img.save(filename, type, quality=quality, optimize=True, progressive=True)
+
+
+if __name__ == "__main__":
+    image_dimensions = (1440, 1440)
+    circle_padding = 40
+    circle_color = '#29C4AF'
+    background_color = 'white'
+    # fnt = ImageFont.truetype('./Raleway-Bold.ttf', 500)
+    fnt = ImageFont.truetype('/Library/Fonts/Arial.ttf', 500)
+    text_color = 'white'
+    type = 'JPEG'
+    quality = 80
+
+    # alphabet = ['a', 'b', 'c', 'ć', 'č', 'd', 'đ', 'dž', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'lj', 'm', 'n',
+    #            'nj', 'o', 'p', 'r', 's', 'š', 't', 'u', 'v', 'z', 'ž', 'q', 'w', 'y', 'x']
+    import string
+
+    alphabet = list(string.ascii_uppercase)
+
+    for c_1 in alphabet:
+        for c_2 in alphabet:
+            text = (c_1 + " " + c_2).upper()
+            filename = './profiles_images/' + text + '.jpeg'
+            generate_profile_image(filename, text, fnt, text_color, image_dimensions, background_color, circle_padding,
+                                   circle_color)
